445c116b4ebdeaad1bb68b92d7a27f2e8bc777cf
shubham shekhar <shubhamshekhar567@gmail.com>
2023-01-03 13:03:52 +0530

    updated api route

--- packages/app/pages/api/hello.ts
+++ packages/app/pages/api/hello.ts
@@ -9,5 +9,8 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  let x = 1;
+
+  res.status(200).json({ name: eval("x===1") })
 }
