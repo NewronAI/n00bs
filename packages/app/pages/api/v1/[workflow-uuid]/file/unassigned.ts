aa73cfd0bc2a2e311265dec1c7cd0e2b86eb3864
shubham shekhar <shubhamshekhar567@gmail.com>
2023-01-26 21:29:26 +0530

    fixed the unassigned files api

--- packages/app/pages/api/v1/[workflow-uuid]/file/unassigned.ts
+++ packages/app/pages/api/v1/[workflow-uuid]/file/unassigned.ts
@@ -1,12 +1,18 @@
 import NextExpress from "@/helpers/node/NextExpress";
 import {db} from "@/helpers/node/db";
 import assertUp from "@/helpers/node/assert/assertUp";
+import {Prisma} from "@prisma/client";
 
 const unassignedFilesApi = new NextExpress();
 
 unassignedFilesApi.get(async (req, res) => {
 
-    const workflowUUID = req.query.workflowUuid as string;
+    const workflowUUID = req.query["workflow-uuid"] as string;
+
+    assertUp(workflowUUID, {
+        status: 400,
+        message: "Workflow UUID is required"
+    });
 
     const workflow = await db.workflow.findFirst({
         where: {
@@ -22,16 +28,22 @@ unassignedFilesApi.get(async (req, res) => {
         message: "Workflow not found"
     });
 
+    const workflowId = workflow.id;
+    console.log(workflowUUID,workflowId);
+
     // As per Srikant and Team, One workflow can have only One task
-    const unassignedFiles = await db.$queryRaw`
-        SELECT workflow_file.*, count(task_assignment.task_id) FROM workflow_file
-            INNER JOIN workflow ON workflow.uuid = ${workflowUUID}
+    const unassignedFiles = await db.$queryRaw`SELECT workflow_file.*, count(task_assignment.task_id) FROM workflow_file
+            INNER JOIN workflow ON workflow.id = ${workflowId}
             LEFT JOIN task_assignment ON workflow_file.id = task_assignment.workflow_file_id
         GROUP BY workflow_file.id, task_assignment.task_id
         HAVING COUNT(task_assignment.task_id) < (SELECT task.min_assignments FROM task
-            INNER JOIN workflow ON workflow.uuid = ${workflowUUID}
+            INNER JOIN workflow ON workflow.id = ${workflowId}
             AND task.workflow_id = workflow.id LIMIT 1)
         ORDER BY workflow_file.id, task_assignment.task_id;`;
+
+
+
+    console.log(unassignedFiles);
 
     res.status(200).json(unassignedFiles);
 
