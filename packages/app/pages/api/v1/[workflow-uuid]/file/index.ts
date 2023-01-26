55b7aed42be7b0ac9a92cb21987ca349c3c0238f
shubham shekhar <shubhamshekhar567@gmail.com>
2023-01-26 19:41:45 +0530

    added received timestamp as per need

--- packages/app/pages/api/v1/[workflow-uuid]/file/index.ts
+++ packages/app/pages/api/v1/[workflow-uuid]/file/index.ts
@@ -7,15 +7,53 @@ const fileApi = new NextExpress();
 
 const pageSize = 10;
 
+// fileApi.get(async (req : NextApiRequest, res : NextApiResponse) => {
+//     // Get all files
+//
+//     const page = Number(req.query.page as string) || 0;
+//     const workflowUuid = req.query?.["workflow-uuid"] as string;
+//
+//     const files = await db.workflow_file.findMany({
+//         skip: page * pageSize,
+//         take: pageSize,
+//         where: {
+//             workflow: {
+//                 uuid: workflowUuid
+//             }
+//         }
+//     });
+//
+//     const count = await db.workflow_file.count({
+//         where: {
+//             status: obj_status.active,
+//             workflow: {
+//                 uuid: workflowUuid
+//             }
+//         }
+//     });
+//
+//
+//     res.status(200).json({
+//         data: files,
+//         pageInfo: {
+//             currentPage: page,
+//             pageSize: pageSize,
+//             totalPages: Math.ceil(count / pageSize),
+//             totalItems: count,
+//             nextAvailable: page < Math.ceil(count / pageSize),
+//             prevAvailable: page > 0
+//         }
+//     });
+//
+// });
+
+
 fileApi.get(async (req : NextApiRequest, res : NextApiResponse) => {
     // Get all files
 
-    const page = Number(req.query.page as string) || 0;
     const workflowUuid = req.query?.["workflow-uuid"] as string;
 
     const files = await db.workflow_file.findMany({
-        skip: page * pageSize,
-        take: pageSize,
         where: {
             workflow: {
                 uuid: workflowUuid
@@ -23,28 +61,8 @@ fileApi.get(async (req : NextApiRequest, res : NextApiResponse) => {
         }
     });
 
-    const count = await db.workflow_file.count({
-        where: {
-            status: obj_status.active,
-            workflow: {
-                uuid: workflowUuid
-            }
-        }
-    });
+    res.status(200).json(files);
 
-
-    res.status(200).json({
-        data: files,
-        pageInfo: {
-            currentPage: page,
-            pageSize: pageSize,
-            totalPages: Math.ceil(count / pageSize),
-            totalItems: count,
-            nextAvailable: page < Math.ceil(count / pageSize),
-            prevAvailable: page > 0
-        }
-    });
-
-});
+})
 
 export default fileApi.handler;
