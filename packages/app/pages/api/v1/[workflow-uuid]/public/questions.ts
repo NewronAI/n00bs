0275f2619e2b8e8b800823beb3e70a2a4fc13d97
shubham shekhar <shubhamshekhar567@gmail.com>
2023-02-13 16:03:14 +0530

    added questions api

---
+++ packages/app/pages/api/v1/[workflow-uuid]/public/questions.ts
@@ -0,0 +1,44 @@
+import NextExpress from "@/helpers/node/NextExpress";
+import assertUp from "@/helpers/node/assert/assertUp";
+import getPublicWorkflowAPISecret from "@/helpers/getPublicWorkflowAPISecret";
+import {db} from "@/helpers/node/db";
+
+const questionsApi = new NextExpress();
+
+
+questionsApi.get(async (req, res) => {
+
+    const workflowUuid = req.query?.["workflow-uuid"] as string;
+
+    assertUp(workflowUuid, {
+        message: "Workflow UUID: Param is required. Should contain the uuid of the workflow",
+        status: 400
+    });
+
+    const secret = req.query?.secret as string;
+
+    assertUp(secret, {
+        message: "Secret: Param is required. Should contain the secret of the workflow",
+        status: 400
+    });
+
+    const questions = await db.question.findMany({
+        where: {
+            task_question: {
+                some: {
+                    task: {
+                        workflow: {
+                            uuid: workflowUuid
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    res.status(200).json(questions);
+
+});
+
+
+export default questionsApi.handler;
