f6542017721dabc3b90ba22974e37e3431610c2c
shubham shekhar <shubhamshekhar567@gmail.com>
2023-03-22 14:18:19 +0530

    added force reassign button

--- packages/app/src/components/renderer/FilenameRenderer.tsx
+++ packages/app/src/components/renderer/FilenameRenderer.tsx
@@ -1,8 +1,4 @@
 import React, {useMemo, useRef} from 'react';
-import {Prisma} from "@prisma/client";
-import axios from "axios";
-import {Tooltip} from "react-tooltip";
-import {IdentificationIcon} from "@heroicons/react/outline";
 import {toast} from "react-toastify";
 
 
