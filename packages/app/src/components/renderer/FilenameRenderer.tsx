feb3eb0a4065a223ccd5746affe711616e5070a0
shubham shekhar <shubhamshekhar567@gmail.com>
2023-03-22 14:18:08 +0530

    added force reassign button

--- packages/app/src/components/renderer/FilenameRenderer.tsx
+++ packages/app/src/components/renderer/FilenameRenderer.tsx
@@ -3,15 +3,21 @@ import {Prisma} from "@prisma/client";
 import axios from "axios";
 import {Tooltip} from "react-tooltip";
 import {IdentificationIcon} from "@heroicons/react/outline";
+import {toast} from "react-toastify";
 
 
 const FilenameRenderer = ({value} : {value : string}) => {
 
     const fileName = useMemo(() => value?.split("/").pop(), [value]);
 
+    const onCLickCopy = async () => {
+        await navigator.clipboard.writeText(value);
+        toast("Copied to clipboard", {type: "success", autoClose: 400});
+    }
+
 
     return (
-        <div>
+        <div onClick={onCLickCopy}>
             {fileName}
         </div>
     );
