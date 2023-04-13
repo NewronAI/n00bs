f42f608bf11653d2f231d45ad59247ddb371d294
shubham shekhar <shubhamshekhar567@gmail.com>
2023-04-14 02:22:50 +0530

    adding GitHub action workflow to deploy on GCP cloud run

--- packages/app/src/components/layouts/DashboardLayout.js
+++ packages/app/src/components/layouts/DashboardLayout.js
@@ -220,11 +220,13 @@ export default function DashboardLayout({
                                 secondaryNav
                             }
                         </div>
-                        <div className="mx-auto max-w-[1350px] ml-2 mr-2 flex-1">
-                            {/* Replace with your content */}
-                            {
-                                children
-                            }
+                        <div className={"w-full flex justify-center"}>
+                            <div className="mx-auto max-w-[1350px] ml-2 mr-2 flex-1">
+                                {/* Replace with your content */}
+                                {
+                                    children
+                                }
+                            </div>
                         </div>
                     </main>
                 </div>
