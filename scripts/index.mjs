be4f7bc0582e6bed8f8aa5d04a8065beb3f84489
AnshulNewron <anshul@newron.ai>
2023-04-18 19:43:12 +0530

    trial 6

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -4,44 +4,44 @@ const path = require("path");
 const child_process = require("child_process");
 const Papa = require("papaparse");
 
-const readdir = promisify(fs.readdir);
+// const readdir = promisify(fs.readdir);
 // const exec = promisify(child_process.exec);
 
 // Read the config file and extract the directory paths.
 const configFilePath = path.join(__dirname, "config.json");
 const config = JSON.parse(fs.readFileSync(configFilePath));
 
-console.log(config.baseLocation, config.imagesDirPath, config.videosDirPath, config.csvFilePath)
+// console.log(config.baseLocation, config.imagesDirPath, config.videosDirPath, config.csvFilePath)
 
 const baseLocation = config.baseLocation;
 const imagesDirPath = config.imagesDirPath;
 const videosDirPath = config.videosDirPath;
 const csvFilePath = config.csvFilePath;
 
-function extractFileInfo(filename) {
-  const parts = filename.split("_");
-//   const state = parts[2];
-//   const district = parts[3];
-//   const speakerID = parts[4];
-//   const utteranceID = parts[5].split("-")[0];
-  console.log(parts)
-  console.log(state, district, speakerID, utteranceID)
-  return null;
-}
+// function extractFileInfo(filename) {
+//   const parts = filename.split("_");
+// //   const state = parts[2];
+// //   const district = parts[3];
+// //   const speakerID = parts[4];
+// //   const utteranceID = parts[5].split("-")[0];
+//   console.log(parts)
+//   console.log(state, district, speakerID, utteranceID)
+//   return null;
+// }
 
-async function findImageFile(imageName) {
-//   const files = await readdir(imagesDirPath);
-//   const matchingFile = files.find((file) => file.includes(imageName));
-//   if (matchingFile) {
-//     return path.join(imagesDirPath, matchingFile);
-//   } else {
-//     return null;
-//   }
-}
+// async function findImageFile(imageName) {
+// //   const files = await readdir(imagesDirPath);
+// //   const matchingFile = files.find((file) => file.includes(imageName));
+// //   if (matchingFile) {
+// //     return path.join(imagesDirPath, matchingFile);
+// //   } else {
+// //     return null;
+// //   }
+// }
 
-async function createVideoFile(audioFilePath, imageFilePath, outputFilePath) {
-  await exec(`ffmpeg -loop 1 -i ${imageFilePath} -i ${audioFilePath} -c:v libx264 -tune stillimage -c:a copy -shortest ${outputFilePath}`);
-}
+// async function createVideoFile(audioFilePath, imageFilePath, outputFilePath) {
+//   await exec(`ffmpeg -loop 1 -i ${imageFilePath} -i ${audioFilePath} -c:v libx264 -tune stillimage -c:a copy -shortest ${outputFilePath}`);
+// }
 
 const csvContents = await fs.readFile(csvFilePath, 'utf-8')
 
