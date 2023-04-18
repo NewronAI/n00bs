70e2d4b94fd7ecc8889a98b4faa670fdadfed366
AnshulNewron <anshul@newron.ai>
2023-04-18 20:49:16 +0530

    trial 8

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -7,25 +7,23 @@ const Papa = require("papaparse");
 // const readdir = promisify(fs.readdir);
 // const exec = promisify(child_process.exec);
 
-// Read the config file and extract the directory paths.
 const configFilePath = path.join(__dirname, "config.json");
 const config = JSON.parse(fs.readFileSync(configFilePath));
-
-// console.log(config.baseLocation, config.imagesDirPath, config.videosDirPath, config.csvFilePath)
 
 const baseLocation = config.baseLocation;
 const imagesDirPath = config.imagesDirPath;
 const videosDirPath = config.videosDirPath;
 const csvFilePath = config.csvFilePath;
 
-function extractFileInfo(filename) {
+function extractImageName(filename) {
   const parts = filename.split("_");
-  const state = parts[2];
-  const district = parts[3];
-  const speakerID = parts[4];
-  const utteranceID = parts[5].split("-")[0];
+  const state = parts[0];
+  const district = parts[1];
+  const speakerID = parts[2];
+  const utteranceID = parts[3].split("-")[0];
+  const imageName = parts[4] + "_" + parts[5];
   console.log(parts)
-  console.log(state, district, speakerID, utteranceID)
+  console.log(state, district, speakerID, utteranceID, imageName)
   return {state, district, speakerID, utteranceID};
 }
 
@@ -53,17 +51,7 @@ for (const row of csvData) {
     const fileLocation = fileDetails.substring(0, separatorIndex);
     const fileName = fileDetails.substring(separatorIndex + 1);
 
-    const {state, district, speakerID, utteranceID} = extractFileInfo(fileName)
-    console.log("State", state, "district", district, "speakerID", speakerID, "utteranceID", utteranceID)
+    const {state, district, speakerID, utteranceID, imageName} = extractFileInfo(fileName)
+    console.log("State", state, "district", district, "speakerID", speakerID, "utteranceID", utteranceID, "Image Name", imageName)
 
-
-//   if (imageFilePath) {
-//     // If an image file was found, create the video file.
-//     const outputFilename = `${state}_${district}_${speakerID}_${utteranceID}.mp4`;
-//     const outputFilePath = path.join(videosDirPath, outputFilename);
-//     await createVideoFile(audioFilePath, imageFilePath, outputFilePath);
-//     console.log(`Created video file: ${outputFilePath}`);
-//   } else {
-//     console.log(`Could not find image file for audio file: ${audioFilename}`);
-//   }
 }
