b1115555efa393387ff593d25ad12ec2a40bb48c
AnshulNewron <anshul@newron.ai>
2023-04-20 16:30:39 +0530

    trial 11

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -122,7 +122,7 @@ for (const row of csvData) {
     }
 
     if(checkAudioFile && checkImageFile) {
-      const checkVideoFile = await createVideoFile(`${baseLocation}/${fileDetails}`,`${imagesDirPath}`,`${videosDirPath}`)
+      const checkVideoFile = await createVideoFile(fileName, `${baseLocation}/${fileDetails}`,`${imagesDirPath}`,`${videosDirPath}`)
       if(checkVideoFile) {
         console.log("Created")
       }
