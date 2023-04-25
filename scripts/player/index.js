d62c45554f12df9d2187528ade06e216f81ad02a
shubham shekhar <shubhamshekhar567@gmail.com>
2023-04-25 22:14:30 +0530

    added image and audio support

--- scripts/player/index.js
+++ scripts/player/index.js
@@ -1,23 +1,46 @@
 
-console.log('script running');
+console.log('Parsing image and audio file paths and adding them to the DOM');
 
 function addVideoElement () {
+
+    console.log('Page loaded, adding video element to DOM');
+
+    // Format of links required : http://{server url / ip}/?a={path of audio from root}&i={path of image from root}
+
     const usp = new URLSearchParams(window.location.search);
-    const pathToFile = usp.get('p');
+    const pathToFile = usp.get('a');
+    const imageFile = usp.get('i');
 
-    console.log(pathToFile);
+    console.log({imageFile});
 
+    const container = document.getElementById("main");
 
-    if(pathToFile){
-        const videoElem = document.createElement('video');
-        videoElem.src = pathToFile;
-        videoElem.autoplay = true;
+    if(imageFile) {
+        const imageElement = document.createElement('img');
         // width="320" height="240" controls
-        videoElem.width = 320;
-        videoElem.height = 240;
-        videoElem.controls = true;
+        imageElement.src = imageFile;
+        imageElement.width = 320;
+        imageElement.height = 240;
+        imageElement.alt = 'Image ' + imageFile;
+        container.appendChild(imageElement);
+    }
 
-        document.body.appendChild(videoElem);
+    console.log({pathToFile});
+
+    if(pathToFile) {
+        const audioElement = document.createElement('audio');
+        audioElement.src = pathToFile;
+        audioElement.autoplay = true;
+
+        audioElement.controls = true;
+        container.appendChild(audioElement);
+
+    }
+
+    if(!pathToFile && !imageFile) {
+        const errorElement = document.createElement('p');
+        errorElement.innerText = 'No file path provided';
+        container.appendChild(errorElement);
     }
 }
 
