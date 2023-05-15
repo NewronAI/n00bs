13951bb11eb63fc3bdc87a3bbe12a08d77ae773f
AnshulNewron <anshul@newron.ai>
2023-05-16 02:09:43 +0530

    Added token

--- packages/app/pages/api/v1/bhashachatbot/webhook.ts
+++ packages/app/pages/api/v1/bhashachatbot/webhook.ts
@@ -4,19 +4,20 @@ import {db} from "@/helpers/node/db";
 import {events, request_method} from "@prisma/client";
 
 const webhook = new NextExpress();
+const myToken = "2d464c63-249b-4c91-8698-45abda5d3b7b"
 
 webhook.get(async (req, res) => {
     const mode = req.query["hub.mode"]
     const challange = req.query["hub.challange"]
     const token = req.query["hub.token"]
 
-    const myToken = "2d464c63-249b-4c91-8698-45abda5d3b7b"
-
     if(mode === "subscribe" && token === myToken) {
-        res.status(200).json(challange);
+        res.status(200).send(challange);
     }
     else {
-        res.status(403).json(challange);
+        res.status(403).json({
+            message: "Failed"
+        });
     }
 })
 
