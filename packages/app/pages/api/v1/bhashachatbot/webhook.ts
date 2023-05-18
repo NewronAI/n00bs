598555f6e762dac86cac074348bc10ab7eb88c6e
AnshulNewron <anshul@newron.ai>
2023-05-19 02:23:37 +0530

    webhook test n and n tends to infinity

--- packages/app/pages/api/v1/bhashachatbot/webhook.ts
+++ packages/app/pages/api/v1/bhashachatbot/webhook.ts
@@ -63,7 +63,7 @@ webhook.post(async (req, res) => {
     const waID = data.entry?.[0]?.changes?.[0]?.value.contacts?.[0]?.wa_id;
     const message = data.entry?.[0]?.changes?.[0]?.value.messages?.[0];
     const textBody: string = message?.type === "interactive" ? message?.interactive?.button_reply?.title : message?.text?.body;
-    const messageId = message?.type === "interactive" ? data.entry?.[0]?.changes?.[0]?.value?.messages?.[0].interactive?.button_reply?.id : null;
+    const messageId = message?.type === "interactive" ? data.entry?.[0]?.changes?.[0]?.value?.messages?.[0].interactive?.button_reply?.id : undefined;
 
     console.log("Extracted important variables", waID, message, textBody, messageId);
     
