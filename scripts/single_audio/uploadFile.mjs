924737fb79d9f0962990a40ade47e5601cc0e5ac
AnshulNewron <anshul@newron.ai>
2023-07-07 13:55:29 +0530

    Upload dummy files

---
+++ scripts/single_audio/uploadFile.mjs
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+const directoryPath = '/data2/data_nginx/single_audio/dummy'; // Replace with the actual directory path
+
+function getFileLink(relativePath) {
+  // Implement your logic to generate the file link based on the relative path
+  // For example, if the files are accessible through a web server, you can prepend the server URL to the relative path
+  return `https://example.com/files/${relativePath}`;
+}
+
+function createLinksAndPostRequest(directoryPath) {
+  const filesData = [];
+
+  // Read the state folders in the directory
+  const stateFolders = fs.readdirSync(directoryPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+  // Process each state folder
+  stateFolders.forEach((state) => {
+    const statePath = path.join(directoryPath, state);
+
+    // Read the district folders in the state folder
+    const districtFolders = fs.readdirSync(statePath, { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name);
+
+    // Process each district folder
+    districtFolders.forEach((district) => {
+      const districtPath = path.join(statePath, district);
+
+      // Read the audio files in the district folder
+      const audioFiles = fs.readdirSync(districtPath)
+        .filter((file) => path.extname(file) === '.wav');
+
+      // Process each audio file
+      audioFiles.forEach((file) => {
+        const filePath = path.join(districtPath, file);
+        const fileLink = getFileLink(path.relative(directoryPath, filePath));
+
+        // Prepare the data object for the POST request
+        const fileData = {
+          file_name: file,
+          file_type: 'audio',
+          file: fileLink,
+          district: district,
+          state: state
+        };
+
+        filesData.push(fileData);
+      });
+    });
+  });
+
+  // Prepare the request body
+//   const requestBody = {
+//     secret: '636eebcef989e94113c5d91c6b493cbd3a17c8df5737fcb7bce7fe90f03787c3',
+//     data: filesData
+//   };
+
+// Make the POST request
+//   axios.post('https://qc.artpark.in/api/v1/2ddba7f3-798c-40bb-b687-d25a7180982a/public/file', requestBody)
+//     .then((response) => {
+//       console.log('POST request successful:', response.data);
+//     })
+//     .catch((error) => {
+//       console.error('Error making POST request:', error);
+//     });
+  console.log("Making request", filesData)
+}
+
+// Call the function with the directory path
+createLinksAndPostRequest(directoryPath);
