eeb873854cf1225b1045d8439d8547a78073c590
AnshulNewron <anshul@newron.ai>
2023-07-12 13:45:19 +0530

    Ingestion script

--- scripts/single_audio/ingestFiles.mjs
+++ scripts/single_audio/ingestFiles.mjs
@@ -17,37 +17,40 @@ async function createLinksAndPostRequest() {
     let i = 0;
     for (const row of csvData) {
         if (i !== 0 && i !== 1) {
+            const file_duration = row[4]
+            console.log("Type of file duration", typeof file_duration)
+            console.log("Type", typeof float(row[4]) )
             const fileData = {
                 file_name: row[2],
                 file_type: 'audio',
                 file: row[3],
                 district: row[1],
                 state: row[0],
-                file_duration: parseFloat(row[4]),
+                file_duration: float(row[4]),
                 vendor: vendor
             };
             filesData.push(fileData);
         }
 
-        if(i === 1000 && i === (filesData.length - 1)) {
-            // console.log("Making request", filesData)
-            const requestBody = {
-              secret: '636eebcef989e94113c5d91c6b493cbd3a17c8df5737fcb7bce7fe90f03787c3',
-              data: filesData
-            };
+        // if(i === 1000 && i === (filesData.length - 1)) {
+        //     // console.log("Making request", filesData)
+        //     const requestBody = {
+        //       secret: '636eebcef989e94113c5d91c6b493cbd3a17c8df5737fcb7bce7fe90f03787c3',
+        //       data: filesData
+        //     };
 
-            const response = await axios.post('https://qc.artpark.in/api/v1/2ddba7f3-798c-40bb-b687-d25a7180982a/public/file', requestBody)
-              .then((response) => {
-                console.log('POST request successful:', response.data);
-              })
-              .catch((error) => {
-                console.error('Error making POST request:', error);
-              });
-            console.log(response);
-            console.log("Successfull");
-            filesData = []
-            i = 0;
-        }
+        //     const response = await axios.post('https://qc.artpark.in/api/v1/2ddba7f3-798c-40bb-b687-d25a7180982a/public/file', requestBody)
+        //       .then((response) => {
+        //         console.log('POST request successful:', response.data);
+        //       })
+        //       .catch((error) => {
+        //         console.error('Error making POST request:', error);
+        //       });
+        //     console.log(response);
+        //     console.log("Successfull");
+        //     filesData = []
+        //     i = 0;
+        // }
         i++;
     }
 }
