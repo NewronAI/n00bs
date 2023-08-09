ee0fa5f5fb161df6473d13d9ea24fc4c8b13dd6a
AnshulNewron <anshul@newron.ai>
2023-08-09 21:39:30 +0530

    Modified script to create csv for missing images

--- scripts/single_audio/generateCSV.mjs
+++ scripts/single_audio/generateCSV.mjs
@@ -4,4 +4,4 @@ const vendor = process.argv[5];
 
 await $`zx ./createConfigFlies.mjs ${csvFilename} ${batch} ${vendor}`;
 
-await $`zx ./index.mjs /data2/data_nginx/single_audio/${vendor}/configFiles/${batch}_${vendor}_config.json`;
+await $`zx ./index.mjs /data2/data_nginx/single_audio/${vendor}/configFiles/${batch}_${vendor}_missing_config.json.json`;
