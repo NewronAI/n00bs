f70267efa9061d37568e47e00445cdfa59132d62
AnshulNewron <anshul@newron.ai>
2023-11-10 17:46:04 +0530

    Edge cases

--- scripts/module0/module0.mjs
+++ scripts/module0/module0.mjs
@@ -3,14 +3,18 @@ import { existsSync } from "fs"
 const audioFileName = process.argv[3];
 const imageName = process.argv[4];
 
-if(!existsSync(`${imageName}`)) {
-    console.log(`Could not find the image file.`);
-    exit(1);
+if (!existsSync(`${imageName}`)) {
+    if (imageName !== "NULL") {
+        console.log(`Could not find the image file.`);
+        exit(1);
+    }
 }
 
-if(!existsSync(`${audioFileName}`)) {
-    console.log(`Could not find the audio file.`);
-    exit(1);
+if (!existsSync(`${audioFileName}`)) {
+    if (audioFileName !== "NULL") {
+        console.log(`Could not find the audio file.`);
+        exit(1);
+    }
 }
 
 function getFileLink(fileLocation, imageLocation) {
@@ -24,7 +28,7 @@ function getFileLink(fileLocation, imageLocation) {
     const encodedImageLocation = encodeURIComponent(relevantImageLocation);
     const fileLink = `http://vaani.qc.artpark.in/iisc/?a=${encodedFileLocation}&i=${encodedImageLocation}`
     return fileLink;
-  }
+}
 
 const link = getFileLink(audioFileName, imageName);
 
